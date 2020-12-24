633199de0c1ffeac81ccd363dcfcc6c2d56e6c47
vturbin <v.turbin@hotmail.com>
2020-12-24 20:50:03 +0100

    nested queries

---
+++ company database/nested_queries.sql
@@ -0,0 +1,23 @@
+-- Find names of all employees who have
+-- sold over 30000 to a single client
+
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+    SELECT works_with.emp_id
+    FROM works_with
+    WHERE works_with.total_sales > 30000
+);
+
+
+-- Find all clients who are handled by the branch
+-- that Michael Scott manages
+-- Assume you know Michael's ID
+SELECT client.client_name
+FROM client
+WHERE client.branch_id = (
+    SELECT branch.branch_id
+    FROM branch
+    WHERE branch.mgr_id = 102
+    LIMIT 1
+);
